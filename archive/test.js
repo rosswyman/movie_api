060d450a15480887c866f1229bd4fed909b3edc5
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-05-17 21:39:44 -0500

    Update test.js
    Working through some test code.  Not for production.

--- archive/test.js
+++ archive/test.js
@@ -1,3 +1,14 @@
-console.log('Hello Node!');
+// console.log('Hello Node!');
 
-console.log('Goodbye.');
+// console.log('Goodbye.');
+
+const http = require('http');
+
+http
+	.createServer((request, response) => {
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		response.end('Welcome to my book club!\n');
+	})
+	.listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
