c38cb4674c3dd5faea70ecdbba09b549297c2362
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-05-19 11:04:49 -0500

    Update server.js
    changed response to text/html

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ http
 				throw err;
 			}
 
-			response.writeHead(200, { 'Content-Type': 'text/plain' });
+			response.writeHead(200, { 'Content-Type': 'text/html' });
 			response.write(data);
 			response.end();
 		});
