a228df0a1f30bf1342cfde5dfac7d3aecff73faa
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-03 13:16:10 -0500

    Update index.js
    Added request to get user info by username

--- index.js
+++ index.js
@@ -146,6 +146,7 @@ app.get('/users', (req, res) => {
 });
 
 // Adds a new user
+
 /* User to be submitted in JSON format
 {
 	ID: Integer,
@@ -179,6 +180,19 @@ app.post('/users', (req, res) => {
 		.catch((error) => {
 			console.error(error);
 			res.status(500).send('Error: ' + error);
+		});
+});
+
+// Get details on user by username
+
+app.get('/users/:Username', (req, res) => {
+	Users.findOne({ Username: req.parama.Username })
+		.then((user) => {
+			res.json(user);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
 		});
 });
 
