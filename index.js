59b17faf75371b6f3134d40c97627b9a64601048
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-08 13:54:38 -0500

    Update index.js
    added passport authentication to get all movies request

--- index.js
+++ index.js
@@ -34,16 +34,20 @@ app.get('/', (req, res) => {
 
 // Returns all movies
 
-app.get('/movies', (req, res) => {
-	Movies.find()
-		.then((movies) => {
-			res.status(201).json(movies);
-		})
-		.catch((err) => {
-			console.error(err);
-			res.status(500).send('Error: ' + err);
-		});
-});
+app.get(
+	'/movies',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Movies.find()
+			.then((movies) => {
+				res.status(201).json(movies);
+			})
+			.catch((err) => {
+				console.error(err);
+				res.status(500).send('Error: ' + err);
+			});
+	}
+);
 
 // Returns information on a specific movie
 
