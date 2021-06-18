c94ca2c5ef92c4a428a0b8b584cf5d75502300f7
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-18 09:30:53 -0500

    Update models.js
    Previously was missing function call

--- models/models.js
+++ models/models.js
@@ -29,7 +29,7 @@ userSchema.statics.hashPassword = (password) => {
 	return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = (password) => {
+userSchema.methods.validatePassword = function (password) {
 	return bcrypt.compareSync(password, this.Password);
 };
 
