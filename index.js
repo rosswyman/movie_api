22c862965190a2df40f18a44f3940d250e3e4116
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-21 14:02:17 -0500

    Revert "Update index.js"
    This reverts commit 8dad322a0ac66ff2d4aa89e863b9c053ebb9cc5e.

--- index.js
+++ index.js
@@ -198,31 +198,20 @@ app.post(
 
 // Get details on user by username
 
-// app.get(
-// 	'/users/:Username',
-// 	passport.authenticate('jwt', { session: false }),
-// 	(req, res) => {
-// 		Users.findOne({ Username: req.params.Username })
-// 			.then((user) => {
-// 				res.json(user);
-// 			})
-// 			.catch((err) => {
-// 				console.error(err);
-// 				res.status(500).send('Error: ' + err);
-// 			});
-// 	}
-// );
-
-app.get('/users/:Username', (req, res) => {
-	Users.findOne({ Username: req.params.Username })
-		.then((user) => {
-			res.json(user);
-		})
-		.catch((err) => {
-			console.error(err);
-			res.status(500).send('Error: ' + err);
-		});
-});
+app.get(
+	'/users/:Username',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Users.findOne({ Username: req.params.Username })
+			.then((user) => {
+				res.json(user);
+			})
+			.catch((err) => {
+				console.error(err);
+				res.status(500).send('Error: ' + err);
+			});
+	}
+);
 
 // Deletes a user
 
