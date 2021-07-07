022d3f91c2771d8ba8afe41bac9b94cd8b80ef6b
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-07 13:17:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,31 +31,31 @@ app.get('/', (req, res) => {
 });
 // Returns all movies
 // 2021_0629 - Temporarily replacing this request to work on front-end client
-app.get(
-	'/movies',
-	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
-		Movies.find()
-			.then((movies) => {
-				res.status(201).json(movies);
-			})
-			.catch((err) => {
-				console.error(err);
-				res.status(500).send('Error: ' + err);
-			});
-	}
-);
+// app.get(
+// 	'/movies',
+// 	passport.authenticate('jwt', { session: false }),
+// 	(req, res) => {
+// 		Movies.find()
+// 			.then((movies) => {
+// 				res.status(201).json(movies);
+// 			})
+// 			.catch((err) => {
+// 				console.error(err);
+// 				res.status(500).send('Error: ' + err);
+// 			});
+// 	}
+// );
 // BEGIN TEMPORARY NEW REQUEST
-// app.get('/movies', function (req, res) {
-// 	Movies.find()
-// 		.then(function (movies) {
-// 			res.status(201).json(movies);
-// 		})
-// 		.catch(function (error) {
-// 			console.error(error);
-// 			res.status(500).send('Error: ' + error);
-// 		});
-// });
+app.get('/movies', function (req, res) {
+	Movies.find()
+		.then(function (movies) {
+			res.status(201).json(movies);
+		})
+		.catch(function (error) {
+			console.error(error);
+			res.status(500).send('Error: ' + error);
+		});
+});
 // END TEMPORARY NEW REQUEST
 
 // Returns information on a specific movie
